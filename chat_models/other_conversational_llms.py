4d9ade8eaea0206f601a7bba1da80d6f6fc6176b
ALIDmed <meddamz2002@gmail.com>
2024-09-16 11:21:43 +0100

    add huggingface api

--- chat_models/other_conversational_llms.py
+++ chat_models/other_conversational_llms.py
@@ -10,11 +10,18 @@ messages = [
     SystemMessage(content="You are a senior experienced python developper"),
     HumanMessage(content="write a python script to automate email sending")
 ]
-# claude_llm = ChatAnthropic(model='claude-3-opus-20240229')
 
-# res = claude_llm.invoke(messages)
-# print(res)
+print("CLAUDE: ")
+claude_llm = ChatAnthropic(model='claude-3-opus-20240229')
+res = claude_llm.invoke(messages)
+print(res)
 
-gemini_llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro")
+print("GEMINI: ")
+gemini_llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
 res = gemini_llm.invoke(messages)
+print(res)
+
+print("MISTRAL: ")
+llm = HuggingFaceEndpoint(repo_id="mistralai/Mistral-7B-Instruct-v0.2")
+res = llm.invoke(messages)
 print(res)
