b51aa562f394ef310a882c86e26f118b2d538c54
ALIDmed <meddamz2002@gmail.com>
2024-09-16 11:09:20 +0100

    add chat models

--- chat_models/other_conversational_llms.py
+++ chat_models/other_conversational_llms.py
@@ -1,1 +1,20 @@
 from langchain_anthropic import ChatAnthropic
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_huggingface import HuggingFaceEndpoint
+from langchain_core.messages import HumanMessage, SystemMessage
+from dotenv import load_dotenv
+
+load_dotenv()
+
+messages = [
+    SystemMessage(content="You are a senior experienced python developper"),
+    HumanMessage(content="write a python script to automate email sending")
+]
+# claude_llm = ChatAnthropic(model='claude-3-opus-20240229')
+
+# res = claude_llm.invoke(messages)
+# print(res)
+
+gemini_llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro")
+res = gemini_llm.invoke(messages)
+print(res)
