e8bea299188bcbacdb5eded31abb996b75d9640d
ALIDmed <meddamz2002@gmail.com>
2024-10-11 17:28:14 +0100

    add tools decorator

--- 5_agents_and_tools/tools/tool_decorator.py
+++ 5_agents_and_tools/tools/tool_decorator.py
@@ -2,46 +2,31 @@ import os
 from dotenv import load_dotenv
 from langchain import hub
 from langchain.agents import AgentExecutor, create_tool_calling_agent
-from langchain.tools import Tool, StructuredTool
+from langchain.tools import Tool, StructuredTool, tool
 from langchain.pydantic_v1 import BaseModel, Field
 from langchain_google_genai import ChatGoogleGenerativeAI
 
-load_dotenv()
-
+@tool
 def greet_user(name: str) -> str:
     return f"Hello {name}"
 
+class ReverseStringArgs(BaseModel):
+    text: str = Field(description="text to be reversed")
+
+@tool(args_schema=ReverseStringArgs)
 def reverse_string(text: str) -> str:
     return text[::-1]
-
-def concatenate_strings(a: str, b: str) -> str:
-    """Concatenates two strings."""
-    return a + b
 
 class ConcatenateStringsArgs(BaseModel):
     a: str = Field(description="first string")
     b: str = Field(description="second string")
 
-tools = [
-    Tool(
-        name="GreetUser", 
-        func=greet_user, 
-        description="Greets the user by name.",
-    ),
-    Tool(
-        name="ReverseString", 
-        func=reverse_string, 
-        description="Reverses the given string.",
-    ),
-    # Use StructuredTool for more complex functions that require multiple input parameters.
-    # StructuredTool allows us to define an input schema using Pydantic, ensuring proper validation and description.
-    StructuredTool.from_function(
-        func=concatenate_strings, 
-        name="ConcatenateStrings", 
-        description="Concatenates two strings.",
-        args_schema=ConcatenateStringsArgs,  # Schema defining the tool's input arguments
-    ),
-]
+@tool(args_schema=ConcatenateStringsArgs)
+def concatenate_strings(a: str, b: str) -> str:
+    """Concatenates two strings."""
+    return a + b
+
+tools = [greet_user, reverse_string, concatenate_strings]
 llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash", api_key=os.getenv("GOOGLE_API_KEY"))
 prompt = hub.pull("hwchase17/openai-tools-agent")
 agent = create_tool_calling_agent(
@@ -55,8 +40,6 @@ agent_executor = AgentExecutor.from_agent_and_tools(
     verbose=True,
     handle_parsing_errors=True,
 )
-
-# Test the agent with sample queries
 response = agent_executor.invoke({"input": "Greet Alice"})
 print("Response for 'Greet Alice':", response)
 
