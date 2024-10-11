42ee82218e880c5b84fbe399c95d8fd1f3a0e429
ALIDmed <meddamz2002@gmail.com>
2024-10-11 18:55:23 +0100

    add tools deep dive

--- 5_agents_and_tools/tools/3_tools_maximum_control.py
+++ 5_agents_and_tools/tools/3_tools_maximum_control.py
@@ -1,24 +1,29 @@
 import os
-from typing import Type
+from typing import Type, Optional
 
 from dotenv import load_dotenv
 from langchain import hub
 from langchain.agents import AgentExecutor, create_tool_calling_agent
-from langchain.tools import Tool, StructuredTool
-from langchain.pydantic_v1 import BaseModel, Field
+from pydantic import BaseModel, Field
 from langchain_core.tools import BaseTool
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.callbacks.manager import CallbackManagerForToolRun
+from langchain_core.callbacks import BaseCallbackManager
 
 load_dotenv()
 
 class SimpleSearchArgs(BaseModel):
     query: str = Field(description="should be a search query")
 
+class MultiplyNumbersArgs(BaseModel):
+    a: float = Field(description="first number to multiply")
+    b: float = Field(description="second number to multiply")
+
 class SimpleSearchTool(BaseTool):
-    name: str = "simple search"
+    name: str = "simple_search"
     description: str = "Useful for when you need to answer questions about current events."
     args_schema: Type[BaseModel] = SimpleSearchArgs
-
+    
     def _run(self, query: str) -> str:
         from tavily import TavilyClient
 
@@ -27,23 +32,27 @@ class SimpleSearchTool(BaseTool):
 
         results = client.search(query=query)
         return f"Search results for {query}\n\n{results}\n"
-    
-class MultiplyNumbersArgs(BaseModel):
-    a: float = Field(description="first number to multiply")
-    b: float = Field(description="second number to multiply")
+class MultiplyNumbersTool(BaseTool):
+    name: str = "multiply_numbers"
+    description: str = "useful for multiplying two numbers"
+    args_schema: Type[BaseModel] = MultiplyNumbersArgs
 
-class MultipleNumbersTool(BaseTool):
-    name: str = "multiply numbers"
-    description: str = "Useful for when you need to multiply two numbers"
-    args_schema: type[BaseModel] = MultiplyNumbersArgs
+    def _run(
+        self,
+        x: float,
+        y: float,
+    ) -> str:
+        """Use the tool."""
+        result = x * y
+        return f"The product of {x} and {y} is {result}"
 
-    def _run(self, a: float, b:float) -> str:
-        res = a * b
-        return "the product of {a} and {b} is {res}"
+
+    def _run(self, a: float, b: float) -> str:
+        return "the product of {a} and {b} is {a*b}"
     
 tools = [
-    SimpleSearchTool,
-    MultipleNumbersTool
+    SimpleSearchTool(),
+    MultiplyNumbersTool()
 ]
 
 prompt = hub.pull("hwchase17/openai-tools-agent")
